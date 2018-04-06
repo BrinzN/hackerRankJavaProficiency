94be9856e63592f7a9138e4debb98e660dd30160
BrinzN <23201151+BrinzN@users.noreply.github.com>
2018-04-06 09:51:44 +0530

    Create OutputFormatting.java

---
+++ OutputFormatting.java
@@ -0,0 +1,16 @@
+public class OutputFormatting {
+  public static void main(String[] args) {
+              Scanner sc=new Scanner(System.in);
+              System.out.println("================================");
+              for(int i=0;i<3;i++){
+                  String s1=sc.next();
+                  int x=sc.nextInt();
+                  //Complete this line
+                  //System.out.println(s1+" "+x);
+                  System.out.printf("%-15s", s1);
+                  System.out.printf("%03d%n", x);
+              }
+              System.out.println("================================");
+
+          }
+ }
